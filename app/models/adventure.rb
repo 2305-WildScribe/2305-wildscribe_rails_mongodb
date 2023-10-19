aef4740e088e5909f3d539f75fd70226dfa97ae2
Derek Chavez <frrenose17@yahoo.com>
2023-10-19 15:08:31 -0600

    wip: setup Mongodb

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,5 +1,8 @@
-class Adventure < ApplicationRecord
+class Adventure
+  include Mongoid::Document
+  field :title, type: String
+ 
   belongs_to :user
 
-  enum stress_level: ["Very Low", "Low", "Moderate", "High", "Very High"]
+  # enum stress_level: ["Very Low", "Low", "Moderate", "High", "Very High"]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
-class User < ApplicationRecord
-  has_many :adventures
-  has_secure_password
-  validates :email, uniqueness: true, presence: true
-  validates :name, presence: true
-  validates_confirmation_of :password, presence: true
+class User
+  include Mongoid::Document
+  field :name, type: String
+  field :email, type: String
+  field :password, type: String
+  # has_many :adventures
+  # has_secure_password
+  # validates :email, uniqueness: true, presence: true
+  # validates :name, presence: true
+  # validates_confirmation_of :password, presence: true
 
-  has_secure_password
+  # has_secure_password
 end
